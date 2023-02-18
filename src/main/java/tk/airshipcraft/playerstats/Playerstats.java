cf7b3cf7e6c81c4253287f6e45b1688bfbd92e38
zeyad rashed <brwnboy2002@gmail.com>
2023-02-18 09:25:41 -0500

    init

---
+++ src/main/java/tk/airshipcraft/playerstats/Playerstats.java
@@ -0,0 +1,17 @@
+package tk.airshipcraft.playerstats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Playerstats extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
